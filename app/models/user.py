88bcc783f8bd70b41fdf4f173bd4542bf09a915f
Zach <hoschar.zach@gmail.com>
2022-08-30 02:11:35 -0500

    finished setting up initial models

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,7 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 
 class User(db.Model, UserMixin):
@@ -8,6 +9,11 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    bio = db.Column(db.String(250))
+    profile_pic = db.Column(db.String(250))
+    profile_background = db.Column(db.String(250))
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
@@ -26,5 +32,83 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'firstName': self.first_name,
+            'lastName': self.last_name,
+            'bio': self.bio,
+            'profilePic': self.profile_pic,
+            'profileBackground': self.profile_background
+        }
+
+    posts = db.relationship(
+        'Post', back_populates='users', cascade='all, delete')
+    comments = db.relationship(
+        'Comment', back_populates='users', cascade='all, delete')
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(280), nullable=False)
+    poster_id = db.Column(
+        db.String(255), db.ForeignKey('users.id'), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, onupdate=func.now())
+
+    images = db.relationship(
+        'Image', back_populates='posts', cascade='all, delete')
+    comments = db.relationship(
+        'Comment', back_populates='posts', cascade='all, delete')
+
+    def to_dict(self):
+        return {
+            'content': self.content,
+            'authorId': self.poster_id,
+            'createdAt': self.created_at,
+            'updatedAt': self.updated_at,
+            'images': [x.to_dict() for x in self.images],
+            'comments': [x.to_dict() for x in self.comments]
+        }
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(280), nullable=False)
+    poster_id = db.Column(
+        db.Integer, db.ForeignKey('users.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, onupdate=func.now())
+
+    images = db.relationship(
+        'Image', back_populates='comments', cascade='all, delete')
+
+    def to_dict(self):
+        return {
+            'content': self.content,
+            'author': self.poster_id,
+            'postId': self.post_id,
+            'createdAt': self.created_at,
+            'updatedAt': self.updated_at
+        }
+
+
+class Image(db.Model):
+    __tablename__ = 'images'
+
+    id = db.Column(db.Integer, primary_key=True)
+    image_url = db.Column(db.String(255), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'))
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, onupdate=func.now())
+
+    def to_dict(self):
+        return {
+            'imageUrl': self.image_url,
+            'postId': self.post_id,
+            'commentId': self.comment_id
         }
