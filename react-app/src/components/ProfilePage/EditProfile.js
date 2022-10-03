9632c565e4753076a142940938d6eb5d9badeadc
Zach <hoschar.zach@gmail.com>
2022-10-02 22:40:48 -0500

    front end styling done for edit profile modal

--- react-app/src/components/ProfilePage/EditProfile.js
+++ react-app/src/components/ProfilePage/EditProfile.js
@@ -1,6 +1,6 @@
 import { Box, Button, IconButton, Stack, TextField } from "@mui/material";
 import Close from "@mui/icons-material/Close";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function EditProfile({ user, setProfileModalOpen }) {
 
@@ -10,17 +10,86 @@ export default function EditProfile({ user, setProfileModalOpen }) {
     const [name, setName] = useState(user.displayName || '')
     const [bio, setBio] = useState(user.bio || '')
     const [profilePhoto, setProfilePhoto] = useState(user.profilePic || '')
+    const [nameErrorText, setNameErrorText] = useState('')
+    const [bioErrorText, setBioErrorText] = useState('')
 
     const formBorderColor = 'rgb(66, 83, 100)'
     const formHighlightColor = 'rgb(29, 155, 240)'
+    const errorColor = 'rgb(244, 33, 46)'
+
+    const nameInputStyles = {
+        '& .MuiInputLabel-root:focused': { color: 'white' },
+        '& .MuiInputLabel-root': { color: `${formBorderColor}` },
+        '& label.Mui-focused': {
+            color: 'white',
+        },
+        border: 'white',
+
+        '& .MuiOutlinedInput-root': {
+            '&:hover fieldset': {
+                borderColor: nameErrorText ? `${errorColor}` : `${formHighlightColor}`,
+            },
+            color: 'white',
+            '& fieldset': {
+                borderColor: nameErrorText ? `${errorColor}` : `${formBorderColor}`,
+            },
+            '& fieldset:hover': {
+                borderColor: nameErrorText ? `${errorColor}` : `${formBorderColor}`,
+            },
+        },
+    }
+
+    const bioInputStyles = {
+        '& .MuiInputLabel-root:focused': { color: 'white' },
+        '& .MuiInputLabel-root': { color: `${formBorderColor}` },
+        '& label.Mui-focused': {
+            color: 'white',
+        },
+        border: 'white',
+
+        '& .MuiOutlinedInput-root': {
+            color: 'white',
+            '&:hover fieldset': {
+                borderColor: bioErrorText ? `${errorColor}` : `${formHighlightColor}`,
+            },
+            '& fieldset': {
+                borderColor: bioErrorText ? `${errorColor}` : `${formBorderColor}`,
+            },
+            '& fieldset:hover': {
+                borderColor: bioErrorText ? `${errorColor}` : `${formBorderColor}`,
+            },
+        },
+    }
+
+    function handleSubmit() {
+    }
+
+
+    useEffect(() => {
+        if (name.length === 0) {
+            setNameErrorText('Display name is required')
+        } else {
+            setNameErrorText('')
+        }
+    }, [name])
+
+    useEffect(() => {
+        if (bio.length > 250) {
+            setBioErrorText('Bio must be less than 250 characters')
+        } else {
+            setBioErrorText('')
+        }
+    }, [bio])
 
     return (
         <Stack
             alignItems="center"
             spacing={1}
             sx={{
-                width: '100%'
+                width: '100%',
+                paddingBottom: '30px'
             }}
+
         >
             <Box
                 display="flex"
@@ -53,6 +122,8 @@ export default function EditProfile({ user, setProfileModalOpen }) {
                 </Box>
                 <Box>
                     <Button
+                        onClick={handleSubmit}
+                        disabled={nameErrorText || bioErrorText}
                         sx={{
                             backgroundColor: 'rgb(239,243,244)',
                             color: 'black',
@@ -66,6 +137,9 @@ export default function EditProfile({ user, setProfileModalOpen }) {
                             transition: 'background-color .5s',
                             '&:hover': {
                                 backgroundColor: 'rgb(215,219,220)'
+                            },
+                            '&:disabled': {
+                                opacity: .5
                             }
                         }}
                     >
@@ -75,12 +149,14 @@ export default function EditProfile({ user, setProfileModalOpen }) {
 
             </Box>
             <Box
+                m={2}
                 sx={{
                     width: '596px',
                     height: '193px',
                     backgroundPosition: 'center center',
                     backgroundImage: `url(${user.profileBackground})`
                 }}>
+
             </Box>
             <Box
                 height="84px"
@@ -95,6 +171,7 @@ export default function EditProfile({ user, setProfileModalOpen }) {
 
             </Box>
             <Box
+
                 component="form"
                 sx={{
                     color: 'white',
@@ -105,14 +182,19 @@ export default function EditProfile({ user, setProfileModalOpen }) {
                 noValidate>
 
                 <TextField
+                    error={nameErrorText}
+                    helperText={nameErrorText}
                     value={name}
                     label="Display Name"
                     variant="outlined"
                     maxlength="25"
+                    sx={nameInputStyles}
                     onChange={(e) => setName(e.target.value)} />
                 <TextField
                     value={bio}
-                    sx={{ width: '95%' }}
+                    error={bioErrorText}
+                    helperText={bioErrorText}
+                    sx={bioInputStyles}
                     multiline
                     maxRows={4}
                     label="Bio"
