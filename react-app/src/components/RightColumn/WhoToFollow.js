f833d76ffcbcabbac87f34b0fcfa1b9c7482e2e3
Zach <hoschar.zach@gmail.com>
2022-10-05 17:19:25 -0500

    menu styling done, now need to populate it with something

--- react-app/src/components/RightColumn/WhoToFollow.js
+++ react-app/src/components/RightColumn/WhoToFollow.js
@@ -1,5 +1,6 @@
 export default function WhoToFollow() {
     return (
-        <div style={{ position: 'fixed', top: 60, height: '150px', width: '350px' }}></div>
+        // <div style={{ position: 'fixed', top: 60, height: '150px', width: '350px' }}></div>
+        null
     )
 }
