d4f01ac11170c808e57a94e7f6bd9483e7837c1f
Zach <hoschar.zach@gmail.com>
2022-10-06 14:43:55 -0500

    replaced context modal with mui modal, added divider on nav bar, fixed styling on create post modal

--- react-app/src/components/Nav/NavBar.js
+++ react-app/src/components/Nav/NavBar.js
@@ -2,7 +2,7 @@
 import { useHistory, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/session'
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import './navbar.css'
@@ -15,6 +15,11 @@ import IconButton from '@mui/material/IconButton';
 import githubIcon from '../../images/github.svg'
 import linkedinIcon from '../../images/linkedin2.svg'
 import headshot from '../../images/headshot.jpg'
+import Divider from '@mui/material/Divider';
+import Reply from '../ReplyModal/Reply';
+import { Box, Modal } from '@mui/material';
+import CreatePost from '../home/CreatePost';
+
 
 const NavBar = () => {
 
@@ -22,6 +27,8 @@ const NavBar = () => {
   const history = useHistory()
   const dispatch = useDispatch()
   const location = useLocation()
+
+  const [replyModalOpen, setReplyModalOpen] = useState(false)
 
   const onLogout = async (e) => {
     await dispatch(logout());
@@ -40,7 +47,16 @@ const NavBar = () => {
     { text: 'Developer', icon: linkedinIcon, link: 'https://www.linkedin.com/in/zachhoschar/' },
     { text: 'Portfolio', icon: headshot, link: 'https://zachhoschar.com/' }
   ]
-
+  const modalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    borderRadius: '15px',
+    backgroundColor: '#15202b',
+    border: '2px solid #15202b',
+    boxShadow: 24,
+  };
   const svgStyle = {
     width: '26.25px',
     height: '26.25px',
@@ -67,43 +83,52 @@ const NavBar = () => {
   }
 
   return (
-    <div className='navigation'>
-      <div className='navigation-icons-wrapper'>
-        <div>
-          <IconButton sx={buttonStyle} onClick={() => history.push('/home')}>
-            <img style={svgStyle} src={duckLogo} ></img>
-          </IconButton>
-        </div>
-        {/* <div style={{ display: 'flex', alignItems: 'center', padding: '12px' }} >{user.username}</div> */}
-        {buttons.map(btn => (
-          <div className='navbar-item-container'>
-            <div className='navbar-item-button'>
-              <IconButton onClick={btn.onClick} sx={buttonStyle}>
-                <img style={btn.profilepic ? profilePicStyle : svgStyle} src={btn.icon}></img>
+    <>
+      <div className='navigation'>
+        <div className='navigation-icons-wrapper'>
+          <div>
+            <IconButton sx={buttonStyle} onClick={() => history.push('/home')}>
+              <img style={svgStyle} src={duckLogo} ></img>
+            </IconButton>
+          </div>
+          {/* <div style={{ display: 'flex', alignItems: 'center', padding: '12px' }} >{user.username}</div> */}
+          {buttons.map(btn => (
+            <div className='navbar-item-container'>
+              <div className='navbar-item-button'>
+                <IconButton onClick={btn.onClick} sx={buttonStyle}>
+                  <img style={btn.profilepic ? profilePicStyle : svgStyle} src={btn.icon}></img>
+                  <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <div style={{ marginRight: '10px', marginLeft: '20px' }}>{btn.text}</div>
+                  </div>
+                </IconButton>
+              </div>
+            </div>
+          ))}
+          <Divider sx={{ bgcolor: 'white', margin: '5px 0px' }} />
+          {links.map(link => (
+            <a href={link.link} target="_blank" >
+              <IconButton sx={buttonStyle}>
+                <img style={link.profilepic ? profilePicStyle : svgStyle} src={link.icon}></img>
                 <div style={{ display: 'flex', justifyContent: 'center' }}>
-                  <div style={{ marginRight: '10px', marginLeft: '20px' }}>{btn.text}</div>
+                  <div style={{ marginRight: '10px', marginLeft: '20px' }}>{link.text}</div>
                 </div>
               </IconButton>
-            </div>
-          </div>
-        ))}
-        {links.map(link => (
-          <a href={link.link} target="_blank" >
-            <IconButton sx={buttonStyle}>
-              <img style={link.profilepic ? profilePicStyle : svgStyle} src={link.icon}></img>
-              <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <div style={{ marginRight: '10px', marginLeft: '20px' }}>{link.text}</div>
-              </div>
-            </IconButton>
-          </a>
-        ))}
-        <div>
-          <div>
-            <CreatePostModal />
-          </div>
+            </a>
+          ))}
+          <button className='main-quack-button' style={{ width: '225px', height: '52px', fontSize: '17px', marginTop: '10px' }} onClick={() => setReplyModalOpen(true)}>Quack</button>
         </div>
       </div>
-    </div>
+      <Modal
+        open={replyModalOpen}
+        onClose={() => setReplyModalOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modalStyle}>
+          <CreatePost setReplyModalOpen={setReplyModalOpen} />
+        </Box>
+      </Modal>
+    </>
   );
 }
 
