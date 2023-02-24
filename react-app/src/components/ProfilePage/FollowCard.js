6d616fafb70ea3bc18a3d4f0bc8a699953dad7be
Zach <hoschar.zach@gmail.com>
2023-02-24 00:56:21 -0500

    fetch users followers if they aren't already in state (not very efficiently though)

--- react-app/src/components/ProfilePage/FollowCard.js
+++ react-app/src/components/ProfilePage/FollowCard.js
@@ -1,8 +1,33 @@
-import { useSelector } from "react-redux"
+import { set } from "date-fns"
+import { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getUserPosts } from "../../store/posts"
+import Loading from "../Loading"
+
 
 export default function FollowCard({ username }) {
+    const dispatch = useDispatch()
     const selectUser = useSelector(state => state.posts.users[username])
+
+    const [postsLoaded, setPostsLoaded] = useState(false)
+    useEffect(() => {
+        setPostsLoaded(false)
+        if (!selectUser) {
+            dispatch(getUserPosts(username, 'quacks'))
+                .then(data => {
+                    setPostsLoaded(true)
+                })
+                .catch(data => {
+                    window.alert("Error")
+                })
+        }
+        setPostsLoaded(true)
+    }, [selectUser])
+    console.log(selectUser)
     return (
-        <div>{selectUser && selectUser.username}</div>
+        <>
+            {!postsLoaded ? <Loading /> : <div>{username && selectUser && username}</div>
+            }
+        </>
     )
 }
